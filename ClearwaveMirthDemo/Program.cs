c50b0136628fd48bfd34d9290cfec29b99365a93
Joseph Daigle <joseph@cridion.com>
2014-10-01 15:25:02 -0400

    initial commit

---
+++ ClearwaveMirthDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearwaveMirthDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var clearwaveXMLMessage = @"
+<message>
+    <type>A04</type>
+    <organizationId>123</organizationId>
+    <patient>
+        <patientId>123</patientId>
+        <lastName>Doe</lastName>
+        <firstName>John</firstName>
+        <dateOfBirth>19850502</dateOfBirth>
+        <gender>M</gender>
+    </patient>
+</message>
+";
+            using (var service = new ClearwaveMirthService())
+            {
+                service.Url = "http://64.88.177.104:PORT/services/SERVICENAME";  // replace PORT and SERVICENAME from specification
+                var response = service.acceptMessage(clearwaveXMLMessage);
+                Console.WriteLine("Response From Clearwave: " + response);
+                Console.ReadLine();
+            }
+        }
+    }
+}
